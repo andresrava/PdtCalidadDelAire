2431742f30f2607bcc20e9b61fbbbeece1ce80d2
rsimbrelo <rodrigo.simbrelo@estudiantes.utec.edu.uy>
2021-10-15 10:38:57 -0300

    Borro el proyecto viejo

--- PDT_CalidadDelAireEJB/ejbModule/com/entities/Usuario.java
+++ PDT_CalidadDelAireEJB/ejbModule/com/entities/Usuario.java
@@ -10,7 +10,7 @@ import javax.persistence.*;
 @MappedSuperclass
 @Entity
 @Inheritance( strategy = InheritanceType.JOINED )
-public class Usuario implements Serializable {
+public abstract class Usuario implements Serializable {
 
 	
 	private static final long serialVersionUID = 1L;	
