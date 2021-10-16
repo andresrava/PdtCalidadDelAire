56a6ba2020a55b5d8de63b8b5350af543abd0248
andresrava <andres.rava@estudiantes.utec.edu.uy>
2021-10-16 10:12:06 -0300

    Pronto lo básico de Administrador

--- PDT_CalidadDelAireEJB/ejbModule/com/servicios/AdministradorBean.java
+++ PDT_CalidadDelAireEJB/ejbModule/com/servicios/AdministradorBean.java
@@ -1,18 +1,74 @@
 package com.servicios;
 
+import java.util.List;
+
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceException;
+import javax.persistence.TypedQuery;
+
+import com.entities.Administrador;
+import com.exception.ServiciosException;
 
 /**
  * Session Bean implementation class AdministradorBean
  */
 @Stateless
 public class AdministradorBean implements AdministradorBeanRemote {
-
+	@PersistenceContext
+	private EntityManager em;
     /**
      * Default constructor. 
      */
     public AdministradorBean() {
         // TODO Auto-generated constructor stub
     }
+    @Override
+	public void crear(Administrador administrador) throws ServiciosException {
+		try {
+			em.persist(administrador);
+			em.flush();
+		} catch(PersistenceException e) {
+			throw new ServiciosException("No se pudo crear el administrador");
+		}
+		
+	}
+
+	@Override
+	public void actualizar(Administrador administrador) throws ServiciosException {
+		try {
+			em.merge(administrador);
+			em.flush();
+		} catch(PersistenceException e) {
+			throw new ServiciosException("No se pudo actualizar el administrador");
+		}
+		
+	}
+
+	@Override
+	public void borrar(Long id) throws ServiciosException {
+		try {
+			Administrador administrador = em.find(Administrador.class, id);
+			em.remove(administrador);
+			em.flush();
+		} catch (PersistenceException e) {
+			throw new ServiciosException("No se pudo borrrar el administrador");
+		}
+		
+	}
+
+	@Override
+	public List<Administrador> obtenerTodos() {
+		TypedQuery<Administrador> query = em.createQuery("SELECT a FROM Administrador a", Administrador.class);
+		return query.getResultList();
+	}
+
+	@Override
+	public List<Administrador> obtenerTodos(String filtro) {
+		TypedQuery<Administrador> query = em.createQuery("SELECT a FROM Administrador a WHERE a.nombre LIKE :nombre", Administrador.class)
+				.setParameter("nombre", filtro);
+		return query.getResultList();
+	}
 
 }
--- PDT_CalidadDelAireEJB/ejbModule/com/servicios/AdministradorBeanRemote.java
+++ PDT_CalidadDelAireEJB/ejbModule/com/servicios/AdministradorBeanRemote.java
@@ -1,8 +1,17 @@
 package com.servicios;
 
+import java.util.List;
+
 import javax.ejb.Remote;
+
+import com.entities.Administrador;
+import com.exception.ServiciosException;
 
 @Remote
 public interface AdministradorBeanRemote {
-
+	void crear(Administrador administrador) throws ServiciosException;
+	void actualizar(Administrador administrador) throws ServiciosException;
+	void borrar(Long id) throws ServiciosException;
+	List<Administrador> obtenerTodos();
+	List<Administrador> obtenerTodos(String filtro);
 }
